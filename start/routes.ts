c9de6d04ef5ba00ffe023400e58504f938a16250
iRaySpace <irayspacii@gmail.com>
2021-11-26 10:40:07 +0800

    format: routes

--- start/routes.ts
+++ start/routes.ts
@@ -18,78 +18,94 @@
 |
 */
 
-import Route from '@ioc:Adonis/Core/Route'
-import Env from '@ioc:Adonis/Core/Env'
-import Stripe from 'stripe'
+import Route from "@ioc:Adonis/Core/Route";
+import Env from "@ioc:Adonis/Core/Env";
+import Stripe from "stripe";
 
-const stripe = new Stripe(Env.get('STRIPE_KEY'), {
-  apiVersion: '2020-08-27',
+const stripe = new Stripe(Env.get("STRIPE_KEY"), {
+  apiVersion: "2020-08-27",
 });
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
+Route.get("/", async () => {
+  return { hello: "world" };
+});
 
-Route.post('/get_upcoming_invoice', async ({ request, response }) => {
-  const { customer, subscription } = request.body()
+Route.post("/get_upcoming_invoice", async ({ request, response }) => {
+  const { customer, subscription } = request.body();
   try {
-    const invoice = await stripe.invoices.retrieveUpcoming({ customer, subscription })
-    return invoice
+    const invoice = await stripe.invoices.retrieveUpcoming({
+      customer,
+      subscription,
+    });
+    return invoice;
   } catch (err) {
-    return response.status(500).send({ detail: err.code })
+    return response.status(500).send({ detail: err.code });
   }
-})
+});
 
-Route.post('/pay_invoice', async ({ request, response }) => {
-  const { invoice } = request.body()
+Route.post("/pay_invoice", async ({ request, response }) => {
+  const { invoice } = request.body();
   try {
     const paidInvoice = await stripe.invoices.pay(invoice, {
-      paid_out_of_band: true
-    })
-    return paidInvoice
-  } catch (err) {
-    console.log(err)
-    return response.status(500)
-  }
-})
-
-Route.post('/get_subscription', async ({ request, response }) => {
-  const { subscription } = request.body()
-  try {
-    const data = await stripe.subscriptions.retrieve(subscription, { expand: ['latest_invoice'] })
-    return data
-  } catch (err) {
-    console.log(err)
-    return response.status(500)
-  }
-})
-
-Route.post('/get_invoices', async ({ request, response }) => {
-  const { subscription } = request.body()
-  try {
-    const data = await stripe.invoices.list({ subscription, status: "open", expand: ['data.payment_intent'] })
-    return data
-  } catch (err) {
-    console.log(err)
-    return response.status(500)
-  }
-})
-
-Route.post('/downgrade_subscription', async ({ request, response }) => {
-  const { subscription, quantity } = request.body()
-  try {
-    const existingSubscription = await stripe.subscriptions.retrieve(subscription);
-    const firstItem = existingSubscription.items.data[0];
-    const updatedSubscription = await stripe.subscriptions.update(existingSubscription.id, {
-      proration_behavior: 'none',
-      items: [{
-        id: firstItem.id,
-        quantity,
-      }]
+      paid_out_of_band: true,
     });
+    return paidInvoice;
+  } catch (err) {
+    console.log(err);
+    return response.status(500);
+  }
+});
+
+Route.post("/get_subscription", async ({ request, response }) => {
+  const { subscription } = request.body();
+  try {
+    const data = await stripe.subscriptions.retrieve(subscription, {
+      expand: ["latest_invoice"],
+    });
+    return data;
+  } catch (err) {
+    console.log(err);
+    return response.status(500);
+  }
+});
+
+Route.post("/get_invoices", async ({ request, response }) => {
+  const { subscription } = request.body();
+  try {
+    const data = await stripe.invoices.list({
+      subscription,
+      status: "open",
+      expand: ["data.payment_intent"],
+    });
+    return data;
+  } catch (err) {
+    console.log(err);
+    return response.status(500);
+  }
+});
+
+Route.post("/downgrade_subscription", async ({ request, response }) => {
+  const { subscription, quantity } = request.body();
+  try {
+    const existingSubscription = await stripe.subscriptions.retrieve(
+      subscription
+    );
+    const firstItem = existingSubscription.items.data[0];
+    const updatedSubscription = await stripe.subscriptions.update(
+      existingSubscription.id,
+      {
+        proration_behavior: "none",
+        items: [
+          {
+            id: firstItem.id,
+            quantity,
+          },
+        ],
+      }
+    );
     return updatedSubscription;
   } catch (err) {
-    console.log(err)
-    return response.status(500)
+    console.log(err);
+    return response.status(500);
   }
-})
+});
